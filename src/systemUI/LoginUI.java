2bbc1a09308f730437569299beb67dec774c5332
KCodeHacking <44612896+KCodeHacking@users.noreply.github.com>
2019-05-25 22:36:22 +0900

    LoginUI
    passwd 작성시 enter키 사용가능 화면 중앙에 login창 띄게 완료.

--- src/systemUI/LoginUI.java
+++ src/systemUI/LoginUI.java
@@ -5,6 +5,8 @@ import java.awt.event.*;
 import java.io.File;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import user.*;
 
@@ -24,10 +26,18 @@ public class LoginUI extends JFrame {
     private ArrayList<String> password = new ArrayList<>();
     private ArrayList<Student> users;
     private Scanner scan;
+    private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    /**
+     * @wbp.nonvisual location=-29,-36
+     */
+    private final JPanel panel = new JPanel();
 
     public LoginUI() {
+    	setTitle("Login");
+    	setBackground(Color.WHITE);
+    	
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setBounds(100, 100, 302, 205);
+        setBounds((screenSize.width-302)/2, (screenSize.height-205)/2, 302, 205); //100 100 302 205 처음 나오는 UI의 위치와 크기
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         setContentPane(contentPane);
@@ -38,16 +48,16 @@ public class LoginUI extends JFrame {
         contentPane.add(lblLogin);
 
         pwdPassword = new JPasswordField();
-        pwdPassword.setBounds(40, 66, 139, 21);
+        pwdPassword.setBounds(40, 66, 211, 21);
         contentPane.add(pwdPassword);
 
         IDField = new JTextField();
-        IDField.setBounds(40, 35, 139, 21);
-        contentPane.add(IDField);
+        IDField.setBounds(40, 35, 211, 21);
+        contentPane.add(IDField); 
         IDField.setColumns(10);
 
         JButton LoginButton = new JButton("로그인");
-        LoginButton.setBounds(195, 49, 79, 23);
+        LoginButton.setBounds(40, 111, 79, 23); //195 49 79 23
         contentPane.add(LoginButton);
 
         try {
@@ -61,7 +71,7 @@ public class LoginUI extends JFrame {
             ObjectInputStream os = new ObjectInputStream(fs);
             try {
                 users = (ArrayList<Student>)os.readObject();
-            } catch (Exception e) {
+            } catch (Exception e) { 
                 System.out.println("read");
             }
         } catch (Exception e) {
@@ -73,6 +83,11 @@ public class LoginUI extends JFrame {
             password.add(scan.next());
             String buf = scan.next();
         }
+        
+       
+       
+
+       
 
         LoginButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -91,7 +106,7 @@ public class LoginUI extends JFrame {
 
                             UserInterface UI = new UserInterface(users.get(j));
                             UI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                            UI.setBounds(100, 100, 700, 550);
+                            UI.setBounds((screenSize.width-700)/2, (screenSize.height-550)/2, 700, 550);
                             UI.setVisible(true);
                             UI.setResizable(false);
                             break;
@@ -102,20 +117,33 @@ public class LoginUI extends JFrame {
                 }
             }
         });
-
+        //enter action
+        Action action = new AbstractAction()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+            	//action 할 것!
+            	LoginButton.doClick();
+            } 
+        };
+        pwdPassword.addActionListener( action ); 
+        IDField.addActionListener( action );
+       
+        
         JButton button = new JButton("비회원 로그인");
-        button.setBounds(159, 111, 115, 23);
+        button.setBounds(125, 111, 126, 23);
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                dispose();
-
+                dispose(); //종료하는것
+ 
                 UserInterface UI = new UserInterface(new Student(-2,"guest", " ", 0, 0, " ", " "));
                 UI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                UI.setBounds(100, 100, 700, 550);
+                UI.setBounds((screenSize.width-700)/2, (screenSize.height-550)/2, 700, 550);
                 UI.setVisible(true);
                 UI.setResizable(false);
-            }
+            } 
         });
         contentPane.add(button);
     }
