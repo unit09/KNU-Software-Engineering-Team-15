19d526db7f4f9117706c66a50a4b583c74515707
툰라온 <hmt7136@gmail.com>
2019-05-25 17:21:43 +0900

    Added setObject and getObject

--- src/client/Client.java
+++ src/client/Client.java
@@ -5,6 +5,8 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 
 import org.json.simple.JSONObject;
@@ -22,13 +24,25 @@ public class Client {
 	
 	//key 저장된 내용을 읽어온다. 저장된 게 없으면 null을 반환한다.
 	public String getString(String key) {
-		return this.read(key);
+		Object o = this.readObject(key);
+		
+		return o == null ? null : o.toString();
+	}
+	
+	//key 저장된 내용을 읽어온다. 저장된 게 없으면 null을 반환한다.
+	public Object getObject(String key) {
+		return this.readObject(key);
 	}
 	
 	//key에 저장된 값이 기존에 있었건 없었건간에 덮어 씌운다
 	public void setString(String key, String value) {
+		setObject(key, value);
+	}
+
+	//key에 저장된 값이 기존에 있었건 없었건간에 덮어 씌운다
+	public void setObject(String key, Object o) {
 		try {
-			write(key, value);
+			writeObject(key, o);
 		} catch (IOException e) {
 			e.getStackTrace();
 		}
@@ -36,76 +50,98 @@ public class Client {
 	
 	//key와 그에 해당하는 value를 삭제한다
 	public void remove(String key) {
-		String saved = read();
-		if(read(saved, key) != null) { //해당 key로 저장된 값이 있으면
-			JSONObject jsonObject = string2JSONObject(saved);
-			jsonObject.remove(key);
+		JSONObject saved = (JSONObject)readObject();
+		if(saved.get(key) != null) { //해당 key로 저장된 값이 있으면
+			saved.remove(key);
+			this.write(saved);
+		}
+	}
+	
+	//key에 저장되어 있던 데이터 뒤에 새로운 value를 덧붙인다.
+	//addEnter가 true이면 "기존 데이터" + "\n" + "새 데이터"
+	//addEnter가 false이면 "기존 데이터" + "새 데이터"
+	//기존에 저장된 key가 없으면 그냥 setString과 동일하다
+	public void addString(String key, String value, boolean addEnter) {
+		String temp = this.getString(key);
+		
+		try {
+			writeObject(key, (temp == null ? "" : temp) + (temp != null && addEnter ? "\n" : "") + value);
+		} catch (IOException e) {
+			e.getStackTrace();
+		}
+	}
+	
+	private void writeObject(String key, Object o) throws IOException {
+		JSONObject saved;
+		saved = (JSONObject)this.readObject();
+		if(saved == null) { //기존에 저장된 값이 없는 경우
+			saved = new JSONObject();
+		}
+		
+		saved.put(key, o);
+		writeJSONObject(saved);
+	}
+	
+	private void writeJSONObject(JSONObject o) throws IOException {
+		write(o);
+	}
+	
+	private void write(Object o) {
+		if(o == null) {
+			System.out.print("[경고] write(o)에서 value가 null입니다.\n");
+		}
+		
+		OutputStream outputStream = null;
+		ObjectOutputStream objectOutputStream = null;
+		try {
+			outputStream = new FileOutputStream(fileName);
+			objectOutputStream = new ObjectOutputStream(outputStream);
+			
+			objectOutputStream.writeObject(o);
+		} catch(Exception e) {
+			e.getStackTrace();
+		} finally {
 			try {
-				write(jsonObject.toString());
+				outputStream.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			try {
+				objectOutputStream.close();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 	}
-	
-	//key에 저장되어 있던 데이터 뒤에 새로운 value를 덧붙인다.
-	//addEnter true이면 "기존 데이터" + "\n" + "새 데이터"
-	//addEnter false이면 "기존 데이터" + "새 데이터"
-	//기존에 저장된 key가 없으면 그냥 setString과 동일하다
-	public void addString(String key, String value, boolean addEnter) {
-		String temp = this.getString(key);
-		
-		try {
-			write(key, (temp == null ? "" : temp) + (temp != null && addEnter ? "\n" : "") + value);
-		} catch (IOException e) {
-			e.getStackTrace();
-		}
-	}
-	
-	private void write(String key, String value) throws IOException {
-		String saved = this.read();
-		JSONObject jsonObject = string2JSONObject(saved);
-		
-		jsonObject.put(key, value);
-		String message = jsonObject.toString();
-		write(message);
-	}
-	
-	private void write(String value) throws IOException {
-		if(value == null) {
-			System.out.print("[경고] write(value)에서 value가 null입니다.\n");
-		}
-		
-		BufferedOutputStream buffer = null;
-		try {
-			OutputStream outputStream = new FileOutputStream(fileName);
-			buffer = new BufferedOutputStream(outputStream);
-			byte by[] = value.getBytes();
-			buffer.write(by);
-		} catch(Exception e) {
-			e.getStackTrace();
-		} finally {
-			buffer.close();
-		}
-	}
 
 	//저장된 key 없으면 null 리턴
-	private String read(String key) {
-		String saved = read();
+	private Object readObject(String key) {
+		JSONObject saved = (JSONObject)readObject();
+		if(saved == null) {
+			return null;
+		}
 		
-		return read(saved, key);
+		return saved.get(key);
 	}
 	
 	//saved는 파일에 저장된 텍스트 내용이다. saved에서 key값을 가진 value를 읽어온다
 	//저장된 key 없으면 null 리턴
-	private String read(String saved, String key) {
+	private String readString(String saved, String key) {
+		return readObject(saved, key).toString();
+	}
+	
+	//saved는 파일에 저장된 텍스트 내용이다. saved에서 key값을 가진 value를 읽어온다
+	//저장된 key 없으면 null 리턴
+	private Object readObject(String saved, String key) {
 		JSONObject jsonObject = string2JSONObject(saved);
 		Object value = (Object)jsonObject.get(key);
 		
-		return value == null ? null : jsonObject.get(key).toString();
+		return value == null ? null : jsonObject.get(key);
 	}
 	
+	//txt 파일을 그대로 읽어오는 함수
 	private String read() {
 		byte buffer[] = null;
 		try {
@@ -119,6 +155,26 @@ public class Client {
 		}
 		
 		return (buffer == null) ? "" : new String(buffer);
+	}
+	
+	//txt 파일을 그대로 읽어오는 함수
+	private Object readObject() {
+		Object output = null;
+		
+		InputStream fis = null;
+		ObjectInputStream ois = null;
+		
+		try {
+			fis = new FileInputStream(fileName);
+			ois = new ObjectInputStream(fis);
+			
+			output = ois.readObject();
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return output == null ? null : output;
 	}
 	
 	//str에 파싱할 게 없으면 new JSONObject를 return
--- src/client/ClientExample.java
+++ src/client/ClientExample.java
@@ -1,18 +1,58 @@
 package client;
 
+import java.io.Serializable;
+
+import org.json.simple.JSONObject;
+
 public class ClientExample {
 
 	public static void main(String[] args) {
-		Client client = new Client();
-		client.setString("key1", "this is a test");
-		client.setString("key2", "apple");
-		client.addString("key2", "banana", true);
-		client.addString("key2", " is yelllow", false);
-		System.out.println(client.getString("key1"));
-		System.out.println(client.getString("key2"));
+//		Client client = new Client();
+//		client.setString("key1", "this is a test");
+//		client.setString("key2", "apple");
+//		client.addString("key2", "banana", true);
+//		client.addString("key2", " is yellow", false);
+//		System.out.println(client.getString("key1"));
+//		System.out.println(client.getString("key2"));
+//		
+//		client.remove("key2");
+//		System.out.println("key2 값을 지웠습니다.\n" + client.getString("key2"));
 		
-		client.remove("key2");
-		System.out.println("key2 값을 지웠습니다.\n" + client.getString("key2"));
+		Client client = new Client();
+		A a = new A();
+		a.x = 11;
+		
+		A a2 = new A();
+		a2.x = 22;
+		
+		client.setObject("A", a);
+		client.setObject("A2", a2);
+		
+		client.setString("str", "hello world!!");
+		client.addString("str", " hihihihihihihi", false);
+		
+		A aa = (A)client.getObject("A");
+		A aaa = (A)client.getObject("A2");
+		
+		System.out.println(aa.x);
+		System.out.println(aaa.x);
+		
+		System.out.println(client.getString("str"));
+		client.remove("str");
+		System.out.println(client.getString("str"));
+		
+		JSONObject o = new JSONObject();
+		o.put("id", "2015117202");
+		o.put("A", a);
+		
+		client.setObject("student1", o);
+		JSONObject o2 = (JSONObject)client.getObject("student1");
+		System.out.println(o2.get("id"));
+		System.out.println(((A)o2.get("A")).x);
 	}
 
+}
+
+class A implements Serializable {
+	int x;
 }
